a5b99c8a49379391e8476b6479e16a021ead2aac
Jack Peterson <jackhp95@gmail.com>
2017-11-30 06:54:36 -0600

    the feeling of dispair

--- sw.js
+++ sw.js
@@ -1,38 +1,7 @@
-this.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open('v1').then(function (cache) {
-            return cache.addAll([
-                'Admin/PlusOne.css',
-                'Assets/ColorLogo.png',
-                'Assets/PlusOneLogo.svg',
-                'Assets/SeatGeekLogo.svg',
-                'Assets/WhitePlusOneLogo.svg'            
-            ]);
-        })
-    );
-});
-
-this.addEventListener('fetch', function (event) {
-    console.log('Handling fetch event for', event.request.url);
-
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(event.request).then(function (response) {
-            if (response) {
-                console.log('Found response in cache:', response);
-
-                return response;
-            }
-            console.log('No response found in cache. About to fetch from network...');
-
-            return fetch(event.request).then(function (response) {
-                console.log('Response from network is:', response);
-
-                return response;
-            }).catch(function (error) {
-                console.error('Fetching failed:', error);
-
-                throw error;
-            });
+        fetch(event.request).catch(function () {
+            return caches.match(event.request);
         })
     );
 });
