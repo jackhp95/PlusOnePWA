77cd6f7fb0d670c97c3630e32db1e7da63d2c4f2
Jack Peterson <jackhp95@gmail.com>
2018-01-19 12:47:13 -0600

    shrug

--- service-worker.js
+++ service-worker.js
@@ -14,15 +14,15 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v420';
+const PRECACHE = 'precache-v69';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-    'index.html',
+    '/index.html',
     '/',
-    'PlusOnePWA.js',
-    'Admin/plusOne.css'
+    '/PlusOnePWA.js',
+    '/Admin/plusOne.css'
 ];
 
 // The install handler takes care of precaching the resources we always need.
