98a87794f7590a58b283a2bde465fa2eb39b3c96
Jack Peterson <jackhp95@gmail.com>
2018-01-20 01:30:54 -0600

    ???

--- service-worker.js
+++ service-worker.js
@@ -19,10 +19,7 @@ const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-    '/index.html',
-    '/',
-    '/PlusOnePWA.js',
-    '/Admin/plusOne.css'
+    '/'
 ];
 
 // The install handler takes care of precaching the resources we always need.
